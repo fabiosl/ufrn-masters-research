0544b2f061ad3b3c0d876e7690f5811657b52933
Fabio Leal <sousaleal.fabio@gmail.com>
2016-01-24 14:34:53 -0300

    WIP

--- experiments/exp02_util.py
+++ experiments/exp02_util.py
@@ -1,9 +1,7 @@
 import random
 
 def random_tag_name():
-	return random.choice(["The", "functions", "supplied", "by", "this", "module", "are", "actually", "bound", "methods", "of", "a", "hidden", "instance", "of", "the", "random.Random", "class", "You", "can", "instantiate", "your", "own", "instances", "of", "Random", "to", "get", "generators", "that", "dont", "share", "state", "This", "is", "especially", "useful", "for", "multithreaded", "programs", "creating", "a", "different", "instance", "of", "Random", "for", "each", "thread", "and", "using", "the", "jumpahead", "method", "to", "make", "it", "likely", "that", "the", "genera
-
-		ted", "sequences", "seen", "by", "each", "thread", "dont", "overlap"])
+	return random.choice(["The", "functions", "supplied", "by", "this", "module", "are", "actually", "bound", "methods", "of", "a", "hidden", "instance", "of", "the", "random.Random", "class", "You", "can", "instantiate", "your", "own", "instances", "of", "Random", "to", "get", "generators", "that", "dont", "share", "state", "This", "is", "especially", "useful", "for", "multithreaded", "programs", "creating", "a", "different", "instance", "of", "Random", "for", "each", "thread", "and", "using", "the", "jumpahead", "method", "to", "make", "it", "likely", "that", "the", "generated", "sequences", "seen", "by", "each", "thread", "dont", "overlap"])
 
 def random_tag_user():
 	return random.choice(["Aaron", "Aaron", "Abbey", "Abbie", "Abby", "Abdul", "Abe", "Abel", "Abigail", "Abraham", "Abram", "Ada", "Adah", "Adalberto", "Adaline", "Adam", "Adam", "Adan", "Addie", "Adela", "Adelaida", "Adelaide", "Adele", "Adelia", "Adelina", "Adeline", "Adell", "Adella", "Adelle", "Adena", "Adina", "Adolfo", "Adolph", "Adria", "Adrian", "Adrian", "Adriana", "Adriane", "Adrianna", "Adrianne", "Adrien", "Adriene", "Adrienne", "Afton", "Agatha", "Agnes", "Agnus", "Agripina", "Agueda", "Agustin", "Agustina", "Ahmad", "Ahmed"])
